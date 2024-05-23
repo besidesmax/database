edc2ccfe980499ee6c3263d16401bf1259de7835
besidesmax <johannes.oelllerer@iu-study.org>
2024-05-23 23:11:58 +0200

    ad table courses

--- classes.py
+++ classes.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Boolean
 from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
@@ -23,3 +23,36 @@ class Student(Base):
     def __repr__(self):
         return f"{self.students_id} , {self.first_name} , {self.last_name} , \
                  {self.date_of_birth} , {self.address}"
+
+
+class Course(Base):
+    __tablename__ = "courses"
+
+    course_id = Column("course_id", Integer, primary_key=True, nullable=False)
+    name = Column("name", String(50))
+    ects = Column("ects", Integer)
+    active = Column("active", Boolean)
+    optional = Column("optional", Boolean)
+    requirement_1 = Column("requirement_1", ForeignKey("courses.course_id"))
+    requirement_2 = Column("requirement_2", ForeignKey("courses.course_id"))
+    requirement_3 = Column("requirement_3", ForeignKey("courses.course_id"))
+    requirement_4 = Column("requirement_4", ForeignKey("courses.course_id"))
+    requirement_5 = Column("requirement_5", ForeignKey("courses.course_id"))
+
+    def __init__(self, course_id, name, ects, active, optional, requirement_1,
+                 requirement_2, requirement_3, requirement_4, requirement_5):
+        self.course_id = course_id
+        self.name = name
+        self.ects = ects
+        self.active = active
+        self.optional = optional
+        self.requirement_1 = requirement_1
+        self.requirement_2 = requirement_2
+        self.requirement_3 = requirement_3
+        self.requirement_4 = requirement_4
+        self.requirement_5 = requirement_5
+
+    def __repr__(self):
+        return (F"{self.course_id} , {self.name} , {self.ects} , {self.active} , {self.optional} \
+                {self.requirement_1} , {self.requirement_2} , {self.requirement_3} \
+                {self.requirement_4} , {self.requirement_5}")
