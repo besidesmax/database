e65de1f403c471f50815fbccc7d77ac1cb22f2c5
besidesmax <johannes.oelllerer@iu-study.org>
2024-06-06 13:36:37 +0200

    add input for config

--- engine.py
+++ engine.py
@@ -1,20 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.exc import OperationalError
 
-# login-data for the Server TODO change with input or import form data at the end
-hostname = "localhost"
-username = "root"
-# Achtung @ muss mit %40 und / muss mit %2F ersetzt werden
-password = "!9PQ!X%40^1B5ChkwE"
-port = "3306"
-database = "database_university"
+from config import hostname, username, port, database, password_url
 
-# TODO insert Test if Server exist and connection is possible
 # engine for sqlalchemy operations
 engine_create = \
-    create_engine("mysql+pymysql://" + username + ":" + password + "@" + hostname + ":" + port + "/" + database)
+    create_engine("mysql+pymysql://" + username + ":" + password_url + "@" + hostname + ":" + port + "/" + database)
 # engine for sqlalchemy_utils operations
-engine = ("mysql+pymysql://" + username + ":" + password + "@" + hostname + ":" + port + "/" + database)
+engine = ("mysql+pymysql://" + username + ":" + password_url + "@" + hostname + ":" + port + "/" + database)
 
 
 def test_database_connection(engine):
