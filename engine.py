7de372a8e113661d07129d7d4d97d3ab8a8b0adb
besidesmax <johannes.oelllerer@iu-study.org>
2024-06-07 00:33:06 +0200

    update

--- engine.py
+++ engine.py
@@ -1,6 +1,4 @@
 from sqlalchemy import create_engine
-from sqlalchemy.exc import OperationalError
-
 from config import hostname, username, port, database, password_url
 
 # engine for sqlalchemy operations
@@ -8,13 +6,3 @@ engine_create = \
     create_engine("mysql+pymysql://" + username + ":" + password_url + "@" + hostname + ":" + port + "/" + database)
 # engine for sqlalchemy_utils operations
 engine = ("mysql+pymysql://" + username + ":" + password_url + "@" + hostname + ":" + port + "/" + database)
-
-
-def test_database_connection(engine):
-    try:
-        # Attempt to connect to the database
-        connection = engine.connect()
-        connection.close()
-        print("Database connection successful.")
-    except OperationalError as e:
-        raise ConnectionError(f"Failed to connect to the database. Error: {e}")
--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 from sqlalchemy_utils import create_database, database_exists, drop_database
-from engine import engine, engine_create, test_database_connection
+from engine import engine, engine_create
 from classes import Base
 from import_data import import_data_csv, check_csv_files_exist
 from analyse_data import (students_avg_grade_xy_ects, show_plot_frequency_avg_grade, all_students_from_country_xy,
                           courses_from_student_xy, courses_teaches_from_professor_xy)
-
-# test connection to database
-test_database_connection(engine_create)
 
 # Check if DB already exists
 if database_exists(engine):
