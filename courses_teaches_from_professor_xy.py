75fc4825fac9db8cc7ca25194d292afd3aeacc80
besidesmax <johannes.oelllerer@iu-study.org>
2024-06-07 10:58:53 +0200

    add

--- courses_teaches_from_professor_xy.py
+++ courses_teaches_from_professor_xy.py
@@ -3,4 +3,3 @@ from analyse_data import courses_teaches_from_professor_xy
 professors = courses_teaches_from_professor_xy(int(input("Enter professor_id: ")))
 for professor in professors:
     print(professor)
-    
