79f9284b77e2eb461917b32a1f24576a98ade0f0
besidesmax <johannes.oelllerer@iu-study.org>
2024-06-07 10:35:23 +0200

    update

--- config.py
+++ config.py
@@ -1,36 +1,36 @@
 import pandas as pd
 from urllib.parse import quote
 
-confid_default = pd.read_csv("confid_default.csv", dtype=str)
+config_default = pd.read_csv("config_default.csv", dtype=str)
 
 
 # Obtain database connection information from user input
 def get_hostname():
-    hostname = (input(f'Enter hostname (default is: {confid_default.loc[0,"hostname"]})')
-                or confid_default.loc[0, "hostname"])
-    confid_default.loc[0, "hostname"] = hostname
-    return confid_default
+    hostname = (input(f'Enter hostname (default is: {config_default.loc[0, "hostname"]})')
+                or config_default.loc[0, "hostname"])
+    config_default.loc[0, "hostname"] = hostname
+    return config_default
 
 
 def get_port():
-    port = (input(f'Enter port (default is: {confid_default.loc[0,"port"]})')
-            or confid_default.loc[0, "port"])  # Default port is 3306
-    confid_default.loc[0, "port"] = port
-    return confid_default
+    port = (input(f'Enter port (default is: {config_default.loc[0, "port"]})')
+            or config_default.loc[0, "port"])  # Default port is 3306
+    config_default.loc[0, "port"] = port
+    return config_default
 
 
 def get_username():
-    username = (input(f'Enter username (default is: {confid_default.loc[0,"username"]})')
-                or confid_default.loc[0, "username"])  # Default port is root
-    confid_default.loc[0, "username"] = username
-    return confid_default
+    username = (input(f'Enter username (default is: {config_default.loc[0, "username"]})')
+                or config_default.loc[0, "username"])  # Default port is root
+    config_default.loc[0, "username"] = username
+    return config_default
 
 
 def get_password():
-    password = (input(f'Enter password (default is: {confid_default.loc[0,"password"]})')
-                or confid_default.loc[0, "password"])
-    confid_default.loc[0, "password"] = password
-    return confid_default
+    password = (input(f'Enter password (default is: {config_default.loc[0, "password"]})')
+                or config_default.loc[0, "password"])
+    config_default.loc[0, "password"] = password
+    return config_default
 
 
 hostname = get_hostname().loc[0, "hostname"]
@@ -40,4 +40,4 @@ password = get_password().loc[0, "password"]
 password_url = quote(password, safe='')
 database = "database_university"
 
-confid_default.to_csv("confid_default.csv")
+config_default.to_csv("config_default.csv")
